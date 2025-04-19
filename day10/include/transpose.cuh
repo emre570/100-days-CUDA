e32fdfbbd939bccdc73f0d9fb24992b258b4d5bf
Emre Albayrak <70805503+emre570@users.noreply.github.com>
2025-04-19 14:40:27 +0300

    Move day 10 to another repo

--- day10/include/transpose.cuh
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cuda_runtime.h>
-
-__global__ void tiled_transpose_kernel(const float* input, float* output, int width, int height);
-
-// Host fonksiyon
-void launch_transpose(const float* d_input, float* d_output, int width, int height);
